36b10023db692eb54f5a94562c285425890cc5aa
jay6655 <mistry.jay6655@gmail.com>
2021-12-22 22:41:38 +0530

    Init

---
+++ app/src/main/java/com/example/shinewashing/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.shinewashing;
+//Jay chamuda Maa
+//Date : 19/12/2021
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
